57c41b00289026a602506e890fceed8f0b4d76a7
Yogesh <yogesh.grjr@rediffmail.com>
2022-01-03 22:50:45 -0600

    state functionality to update the meme componenet

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,22 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme(){
+    const [url,setUrl] = React.useState("")
+    function onClick(){
+        const randomIndex = Math.floor(Math.random()*memesData.data.memes.length)
+        setUrl(memesData.data.memes[randomIndex].url)
+    }
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="top text" className="form--input" />
                 <input type="text" placeholder="bottom text" className="form--input" />
-                <button className="form--button" >
+                <button className="form--button" onClick={onClick}>
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={url} className="meme--image"/>
         </main> 
     )
 }
