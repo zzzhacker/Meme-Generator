8ed3875d93ebdea9799bf34a0ae1434fc87de5b9
Yogesh <yogesh.grjr@rediffmail.com>
2022-01-04 19:33:25 -0600

    async function with useEffect

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -10,9 +10,13 @@ export default function Meme(){
     const [allMemeImages,setAllMemeImages] = React.useState([])
 
     React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemeImages(data.data.memes))
+        async function getMemes(){
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemeImages(data.data.memes)
+        }
+        getMemes()
+        
     }, [])
 
     function onClick(){
